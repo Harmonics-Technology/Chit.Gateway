45844fc7939d49a034230d586199565a3f35e0c3
adelowomi <adelowomi@gmail.com>
2023-11-01 18:14:34 +0100

    made some progress on on yarp reverse proxy and swagger

--- Chit.Identity/Program.cs
+++ Chit.Identity/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System.Text;
 using Chit.Context;
 using Chit.Identity;
+using Chit.Identity.Utilities;
+using Chit.Utilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Yarp.ReverseProxy.Swagger;
+using Yarp.ReverseProxy.Swagger.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 // load connection string from azure key vault depending on the environment
@@ -21,17 +27,40 @@ builder.Services.AddChitContext(new ChitContextOptions
     ConnectionString = connectionString
 });
 
+// add utilities to the service container
+builder.Services.AddUtilities(builder.Configuration);
+
 builder.Services.AddTransient<IEncryptionService, EncryptionService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 builder.Services.AddSwaggerGen();
 // register service for request encryption
 builder.Services.AddTransient<RequestEncryptionsMiddleware>();
 
+var yarpConfiguration = builder.Configuration.GetSection("ReverseProxy");
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(yarpConfiguration)
+    .AddSwagger(yarpConfiguration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+
+
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -64,22 +93,34 @@ if (app.Environment.IsDevelopment())
             })));
         }
         return next();
-
         // decrypt response for swagger requests
-
     });
-    app.UseSwaggerUI();
+
+    app.UseSwaggerUI(options =>
+    {
+        var config = app.Services.GetRequiredService<IOptionsMonitor<ReverseProxyDocumentFilterConfig>>().CurrentValue;
+        foreach (var cluster in config.Clusters)
+        {
+            options.SwaggerEndpoint($"/swagger/{cluster.Key}/swagger.json", $"{cluster.Key.ToUpper()} Microservice");
+        }
+    });
 }
-
-
 app.UseHttpsRedirection();
+
+// .SetIsOriginAllowed(origin => true)); // allow any origin
+// .AllowCredentials());
+
+app.UseRouting();
 // add a decryption middleware to decrypt the request before it hits the controller
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+
 app.UseRequestEncryptionsMiddleware();
+
+app.MapReverseProxy();
 // add an encryption middleware to encrypt the response before it leaves the controller
 
 app.Run();
